b302da4487466554af8c41f19010f6125d651a93
TilakMaddy <tilak.madichetti@gmail.com>
2025-02-20 13:32:17 +0530

    wip

--- generate_release.sh
+++ generate_release.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Create the vsix package
+npm install
+npx vsce package
+
+# Find the `.vsix` file (adjust to your actual file path or naming convention)
+VSIX_FILE=$(ls *.vsix | head -n 1)
+
+if [ -z "$VSIX_FILE" ]; then
+  echo "No .vsix file found to upload"
+  exit 1
+fi
+
 # Ensure the script is run with a version argument
 if [ -z "$1" ]; then
   echo "Please provide a version (e.g., 1.0.1)"
@@ -16,19 +28,11 @@ RELEASE_BODY="## v$VERSION - Release Notes\n\n### What's New\n\n$CHANGELOG\n\n##
 
 # Create the release using GitHub API
 RESPONSE=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
-  -d "{\"tag_name\":\"v$VERSION\", \"name\":\"Release v$VERSION\", \"body\":\"$RELEASE_BODY\", \"draft\":false, \"prerelease\":false}" \
+  -d "{\"tag_name\":\"v$VERSION\", \"name\":\"Release $VERSION\", \"body\":\"$RELEASE_BODY\", \"draft\":false, \"prerelease\":false}" \
   https://api.github.com/repos/Cyfrin/vscode-aderyn/releases)
 
 # Extract the upload URL from the response
 UPLOAD_URL=$(echo $RESPONSE | jq -r .upload_url | sed -e "s/{?name,label}//")
-
-# Find the `.vsix` file (adjust to your actual file path or naming convention)
-VSIX_FILE=$(ls *.vsix | head -n 1)
-
-if [ -z "$VSIX_FILE" ]; then
-  echo "No .vsix file found to upload"
-  exit 1
-fi
 
 # Upload the .vsix file to the release
 curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
