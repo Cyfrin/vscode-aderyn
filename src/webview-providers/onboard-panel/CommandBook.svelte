a4e95ed3a817d935d7e0b11f1e95dbfc4987e7a8
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-20 18:19:53 +0530

    fix: remove colors

--- src/webview-providers/onboard-panel/CommandBook.svelte
+++ src/webview-providers/onboard-panel/CommandBook.svelte
@@ -14,33 +14,19 @@
 
 <div class="m-4 mt-8 rounded-lg p-6">
     <div class="mb-4">
-        <h1 class="text-2xl font-bold text-center text-gray-800 dark:text-gray-100">
-            Command Guide
-        </h1>
+        <h1 class="text-2xl font-bold text-center">Command Guide</h1>
         <div class="w-16 h-1 mx-auto mt-2"></div>
     </div>
 
     <div class="mb-8">
-        <p class="font-light mt-4 text-gray-600 dark:text-gray-300">
+        <p class="font-light mt-4">
             Remember, to press
-            <kbd
-                class="px-2 py-1 text-xs font-semibold bg-gray-100 dark:bg-gray-700 rounded"
-                >Cmd</kbd
-            >
+            <kbd class="px-2 py-1 text-xs font-semibold rounded">Cmd</kbd>
             /
-            <kbd
-                class="px-2 py-1 text-xs font-semibold bg-gray-100 dark:bg-gray-700 rounded"
-                >Ctrl</kbd
-            >
+            <kbd class="px-2 py-1 text-xs font-semibold rounded">Ctrl</kbd>
             +
-            <kbd
-                class="px-2 py-1 text-xs font-semibold bg-gray-100 dark:bg-gray-700 rounded"
-                >Shift</kbd
-            >
-            <kbd
-                class="px-2 py-1 text-xs font-semibold bg-gray-100 dark:bg-gray-700 rounded"
-                >P</kbd
-            >
+            <kbd class="px-2 py-1 text-xs font-semibold rounded">Shift</kbd>
+            <kbd class="px-2 py-1 text-xs font-semibold rounded">P</kbd>
             and type
             <span class="font-bold text-blue-600 dark:text-blue-400"
                 >"Aderyn: Welcome on board"</span
@@ -54,17 +40,11 @@
             <table class="w-3xl border-collapse">
                 <tbody>
                     {#each Object.entries(commandGuide) as [command, purpose], i}
-                        <tr
-                           
-                        >
-                            <td
-                                class="py-3 px-4 border-b"
-                            >
+                        <tr>
+                            <td class="py-3 px-4 border-b">
                                 Aderyn: {command}
                             </td>
-                            <td
-                                class="py-3 px-4 border-b"
-                            >
+                            <td class="py-3 px-4 border-b">
                                 {@html purpose}
                             </td>
                         </tr>
