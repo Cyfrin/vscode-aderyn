129fd50ab64c3d86b354693bbf988d86900d0673
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-23 15:27:22 +0530

    fix

--- src/commands/actions/start-server.ts
+++ src/commands/actions/start-server.ts
@@ -2,18 +2,14 @@ import * as vscode from 'vscode';
 import { client, startServing } from '../../state';
 
 async function action() {
-    if (!client) {
-        vscode.window.showErrorMessage('aderyn client not found');
-        return;
-    }
     try {
-        if (client.isRunning()) {
+        if (client && client.isRunning()) {
             vscode.window.showWarningMessage('Aderyn diagnostics server is running.');
         } else {
             await startServing();
         }
     } catch (err) {
-        client.error('Starting Aderyn failed', err, 'force');
+        if (client) client.error('Starting Aderyn failed', err, 'force');
     }
 }
 
