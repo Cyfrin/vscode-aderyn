cd51b7b82127c07649a5a0028111193073123a28
TilakMaddy <tilak.madichetti@gmail.com>
2025-04-14 20:20:59 +0530

    remove hardhat remappings warning message

--- src/utils/install/aderyn.ts
+++ src/utils/install/aderyn.ts
@@ -24,30 +24,6 @@ async function isAderynAvailableOnPath(logger: Logger): Promise<boolean> {
 }
 
 async function createAderynReportAndDeserialize(projectRootUri: string): Promise<Report> {
-    // Check if it's a pure hardhat project without remappings.txt present.
-    // If so, make a warning toast
-    const remappingsPath = path.join(projectRootUri, 'remappings.txt');
-    const hhConfigJs = path.join(projectRootUri, 'hardhat.config.js');
-    const hhConfigTs = path.join(projectRootUri, 'hardhat.config.ts');
-    const foundryToml = path.join(projectRootUri, 'foundry.toml');
-    if (fs.existsSync(hhConfigJs) || fs.existsSync(hhConfigTs)) {
-        if (!fs.existsSync(foundryToml) && !fs.existsSync(remappingsPath)) {
-            vscode.window
-                .showWarningMessage(
-                    'It is recommended to create remappings.txt to help Aderyn resolve files in hardhat projects.',
-                    'Learn',
-                )
-                .then((selection) => {
-                    if (selection === 'Learn') {
-                        vscode.env.openExternal(
-                            vscode.Uri.parse(
-                                'https://github.com/Cyfrin/aderyn/discussions/879',
-                            ),
-                        );
-                    }
-                });
-        }
-    }
     const cmd = `aderyn -o report.json --stdout --skip-cloc`;
     // 20 second timeout
     return executeCommand(cmd, undefined, 20 * 1000, projectRootUri)
